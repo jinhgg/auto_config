850a7365e60b6137cf145c5bd072aa8667f5de72
jinhgg <970402782@qq.com>
2020-08-22 22:22:32 +0800

    add vim_config.sh

---
+++ vim_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 安装插件
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
+
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
