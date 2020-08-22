49c1dbb8f298feac746b172ecc4c60dfc0057cdb
jinhgg <970402782@qq.com>
2020-08-22 22:52:42 +0800

    11

--- vim_config.sh
+++ vim_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 安装插件
+# 安装插件2
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
