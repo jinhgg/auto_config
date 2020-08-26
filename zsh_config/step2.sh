de53f0e951e908098db25a03c8f87cd66072aed0
jinhgg <33146207+jinhgg@users.noreply.github.com>
2020-08-26 10:28:02 +0800

    Update step2.sh

--- zsh_config/step2.sh
+++ zsh_config/step2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
 sed -i "s/plugins=(git)/plugins=(\\ngit\\nsudo\\nzsh-syntax-highlighting\\nzsh-autosuggestions\\n)/g" ~/.zshrc
 echo "export PATH=$PATH:~/.local/bin" >> ~/.zshrc
